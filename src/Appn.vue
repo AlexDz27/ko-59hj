d6384bf6e863f3a4c75f387d8a536738565ebc8c
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-11-05 00:39:30 +0300

    w, todo: continue

--- src/Appn.vue
+++ src/Appn.vue
@@ -8,7 +8,6 @@ const titles = {
   untilIndependentWorkFinish: 'Самостоятельная работа. До её конца',
   untilLessonFinish: 'До конца урока',
 }
-
 const currentTitleKey = ref('untilTypingFinish')
 function cycleTitles() {
   const titlesKeys = Object.keys(titles)
@@ -24,9 +23,9 @@ function cycleTitles() {
     <p class="timer__wrap__title" @keyup.enter="cycleTitles" contenteditable>
       {{ titles[currentTitleKey] }}:
     </p>
-
-
-
+    <section class="timer" @keyup.enter="startCountdown">
+      <input v-model="minutesText" class="timer__input timer__input__minutes" placeholder="00">:<input v-model="secondsText" class="timer__input timer__input__seconds" placeholder="00">
+    </section>
     <p class="timer__wrap__subtitle" contenteditable>
       
     </p>
