3c35c395edc7c6c849d1a997d535bf68011ffb22
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-11-04 18:24:56 +0300

    Add logic

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,17 @@ export default {
     return {
       titles: {
         untilLessonStart: 'До начала урока',
+        untilTypingFinish: 'До конца печатания',
         untilBreakFinish: 'До конца перерыва',
+        untilIndependentWorkFinish: 'Самостоятельная работа. До её конца',
+        untilLessonFinish: 'До конца урока',
       },
-      showingTitle: 'untilLessonStart'
+      showingTitle: 'untilLessonStart',
+      minutesText: null,
+      secondsText: null,
+      seconds: 0,
+      futureHours: null,
+      futureMinutes: null,
     }
   },
   
@@ -17,6 +25,35 @@ export default {
       let idxNext = idx + 1; if (idxNext === titlesKeys.length) idxNext = 0
       
       this.showingTitle = titlesKeys[idxNext]
+    },
+    startCountdown() {
+      document.activeElement.blur()
+
+      console.log(this.minutesText, this.secondsText)
+
+      const secondsSummed = Number(this.minutesText) * 60 + Number(this.secondsText)
+      this.seconds = secondsSummed
+      console.log(this.seconds)
+      const interval = setInterval(() => {
+        this.seconds--
+
+        console.log(this.seconds)
+
+        if (this.seconds % 60 < 10) this.secondsText = '0' + this.seconds % 60
+        else this.secondsText = this.seconds % 60
+
+        const minutesRes = Math.trunc(this.seconds / 60)
+        if (minutesRes < 10) this.minutesText = '0' + minutesRes
+        else this.minutesText = minutesRes
+
+        if (this.seconds === 0) clearInterval(interval)
+      }, 1000)
+
+      const now = new Date()
+      const futureTime = new Date(now.getTime() + secondsSummed * 1000)
+      this.futureHours = futureTime.getHours()
+      this.futureMinutes = futureTime.getMinutes()
+      console.log(this.futureHours, this.futureMinutes)
     }
   }
 }
@@ -25,11 +62,14 @@ export default {
 <template>
   <main class="timer__wrap main cont">
     <p class="timer__wrap__title" @dblclick="cycleTitles" contenteditable>
-      <slot>{{ titles[showingTitle] }}</slot>:
+      {{ titles[showingTitle] }}:
     </p>
-    <section class="timer">
-
+    <section class="timer" @keyup.enter="startCountdown">
+      <input v-model="minutesText" class="timer__input timer__input__minutes" placeholder="00">:<input v-model="secondsText" class="timer__input timer__input__seconds" placeholder="00">
     </section>
+    <p v-if="this.futureHours" class="timer__wrap__time-after-countdown">
+      <span class="timer__wrap__time-after-countdown__text">{{ futureHours }} : {{ futureMinutes }}</span>
+    </p>
     <p class="timer__wrap__subtitle" contenteditable>
       
     </p>
