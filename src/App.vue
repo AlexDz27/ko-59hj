4dddc523dc871c377a79aeba74bf8bf428006503
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-11-04 12:46:24 +0300

    Add titles with basic logic

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      titles: {
+        untilLessonStart: 'До начала урока',
+        untilBreakFinish: 'До конца перерыва',
+      },
+      showingTitle: 'untilLessonStart'
+    }
+  },
+  
+  methods: {
+    cycleTitles() {
+      const titlesKeys = Object.keys(this.titles)
+      const idx = titlesKeys.indexOf(this.showingTitle)
+      let idxNext = idx + 1; if (idxNext === titlesKeys.length) idxNext = 0
+      
+      this.showingTitle = titlesKeys[idxNext]
+    }
+  }
+}
+</script>
 
 <template>
   <main class="timer__wrap main cont">
-    <p class="timer__wrap__title" contenteditable>До начала урока:</p>
+    <p class="timer__wrap__title" @dblclick="cycleTitles" contenteditable>
+      <slot>{{ titles[showingTitle] }}</slot>:
+    </p>
     <section class="timer">
 
     </section>
+    <p class="timer__wrap__subtitle" contenteditable>
+      
+    </p>
+    <!-- Можно поиграть в Акинатор -->
   </main>
 </template>
