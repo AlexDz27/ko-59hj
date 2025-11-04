4931802c23d6c7878c9c3f1a7d0e865127e8cbf2
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-11-04 11:35:13 +0300

    i

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <main class="timer__wrap main cont">
+    <p class="timer__wrap__title" contenteditable>До начала урока:</p>
+    <section class="timer">
+
+    </section>
+  </main>
+</template>
